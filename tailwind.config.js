75b64710b2d11582871e9e74e8809818b20a0271
Christof Kälin <christof.kaelin@protonmail.com>
2024-06-17 13:43:27 +0200

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "daily-dev-tips": "#F89283",
+      },
+    },
+  },
+  plugins: [],
+}
